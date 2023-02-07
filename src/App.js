f6cefc38b393a5c6e4bf081ec94cf1eb2c2e0e88
sunraja1996 <sunraja1996@outlook.com>
2023-02-07 16:28:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [fullUrl, setFullUrl] = useState("");
+  const [shortUrls, setShortUrls] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("https://url-shortern-w20r.onrender.com", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ fullUrl }),
+      });
+
+      const data = await response.json();
+
+      setShortUrls([...shortUrls, data]);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <h1 className="text-center">URL Shortern</h1>
+      <form
+        className="form-inline my-4"
+        onSubmit={handleSubmit}
+        action="/shortUrls"
+        method="POST"
+      >
+       
+        <input
+          required
+          placeholder="Url"
+          type="url"
+          name="fullUrl"
+          id="fullUrl"
+          className="form-control col mr-2"
+          value={fullUrl}
+          onChange={(e) => setFullUrl(e.target.value)}
+        />
+        <button className="btn btn-success" type="submit">
+          Shrink
+        </button>
+      </form>
+
+      <table className="table table-striped table-responsive">
+        <thead>
+          <tr>
+            <th>Full URL</th>
+            <th>Short URL</th>
+            <th>Clicks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {shortUrls.map((shortUrl, index) => (
+            <tr key={index}>
+              <td>
+                <a href={shortUrl.full}>{shortUrl.full}</a>
+              </td>
+              <td>
+                <a href={shortUrl.short}>{shortUrl.short}</a>
+              </td>
+              <td>{shortUrl.clicks}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
